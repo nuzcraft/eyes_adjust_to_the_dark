cbfa2fdaf64bc10482d07ebae9d07a179bbe24f4
nuzcraft <nuzcraft@gmail.com>
2019-04-16 07:46:26 -0500

    Moved user defined types, structs, enums, traits into their own file, user_defined.rs #2

---
+++ src/user_defined.rs
@@ -0,0 +1,353 @@
+// this file will hold user defined structs, types, enums, etc
+use tcod::console::*;
+use tcod::colors::{self, Color};
+use tcod::map::{Map as FovMap}; // the 'Map as FovMap' section renames the tcod fov map
+                                // so that it doesn't conflict with our user defined Map
+use tcod::input::{Mouse};
+
+/// structs
+// this is a generic object. Anything represented by a character on the screen
+// player, monster, stairs, item, etc
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Object {
+    pub x: i32,
+    pub y: i32,
+    pub char: char,
+    pub color: Color,
+    pub name: String,
+    pub blocks: bool,
+    pub alive: bool,
+    pub fighter: Option<Fighter>,
+    pub ai: Option<Ai>,
+    pub item: Option<Item>,
+    pub always_visible: bool,
+    pub level: i32,
+    pub equipment: Option<Equipment>,
+}
+
+impl Object {
+    pub fn new(x: i32, y: i32, char: char, name: &str, color: Color, blocks: bool) -> Self {
+        Object {
+            x: x,
+            y: y,
+            char: char,
+            color: color,
+            name: name.into(),
+            blocks: blocks,
+            alive: false,
+            fighter: None,
+            ai: None,
+            item: None,
+            always_visible: false,
+            level: 1,
+            equipment: None,
+        }
+    }
+
+    /// set the color, then draw the character that represents this object at its position
+    pub fn draw(&self, con: &mut Console) {
+        con.set_default_foreground(self.color);
+        con.put_char(self.x, self.y, self.char, BackgroundFlag::None);
+    }
+
+    /// Erase the character that represents this object
+    pub fn clear(&self, con: &mut Console) {
+        con.put_char(self.x, self.y, ' ', BackgroundFlag::None);
+    }
+
+    // returns the current position
+    pub fn pos(&self) -> (i32, i32) {
+        (self.x, self.y)
+    }
+
+    // sets a new position for an object
+    pub fn set_pos(&mut self, x: i32, y: i32) {
+        self.x = x;
+        self.y = y;
+    }
+
+    // return the distance to another object
+    pub fn distance_to(&self, other: &Object) -> f32 {
+        let dx = other.x - self.x;
+        let dy = other.y - self.y;
+        ((dx.pow(2) + dy.pow(2)) as f32).sqrt()
+    }
+
+    pub fn take_damage(&mut self, damage: i32, game: &mut Game) -> Option<i32> {
+        // apply damage if possible
+        if let Some(fighter) = self.fighter.as_mut() {
+            if damage > 0 {
+                fighter.hp -= damage;
+            }
+        }
+        // check for death, call the death function
+        if let Some(fighter) = self.fighter {
+            if fighter.hp <= 0 {
+                self.alive = false;
+                fighter.on_death.callback(self, game);
+                return Some(fighter.xp);
+            }
+        }
+        None
+    }
+
+    pub fn attack(&mut self, target: &mut Object, game: &mut Game) {
+        // a simple formula for attack damage
+        let damage = self.power(game) - target.defense(game);
+        if damage > 0 {
+            // make the target take some damage
+            game.log.add(format!("{} attacks {} for {} hit points", self.name, target.name, damage), colors::WHITE);
+            if let Some(xp) = target.take_damage(damage, game) {
+                // yield experience to the player
+                self.fighter.as_mut().unwrap().xp += xp;
+            }
+        } else {
+            game.log.add(format!("{} attacks {} but it has no effect", self.name, target.name), colors::WHITE);
+        }
+    }
+
+    /// heal by the given amount, without going over the maximum
+    pub fn heal(&mut self, amount: i32, game: &Game) {
+        let max_hp = self.max_hp(game);
+        if let Some(ref mut fighter) = self.fighter {
+            fighter.hp += amount;
+            if fighter.hp > max_hp {
+                fighter.hp = max_hp;
+            }
+        }
+    }
+
+    /// return the distance to some coordinates
+    pub fn distance(&self, x: i32, y: i32) -> f32 {
+        (((x - self.x).pow(2) + (y - self.y).pow(2)) as f32).sqrt()
+    }
+
+    /// equip object and show a message about it
+    pub fn equip(&mut self, log: &mut Vec<(String, Color)>) {
+        if self.item.is_none() {
+            log.add(format!("Can't equip {:?} because it's not an Item.", self),
+                colors::RED);
+            return
+        };
+        if let Some(ref mut equipment) = self.equipment {
+            if !equipment.equipped {
+                equipment.equipped = true;
+                log.add(format!("Equipped {} on {}.", self.name, equipment.slot),
+                    colors::LIGHT_GREEN);
+            }
+        } else {
+            log.add(format!("Can't equip {:?} because it's not an Equipment", self),
+                colors::RED);
+        }
+    }
+
+    /// dequip object and show a message about it
+    pub fn dequip(&mut self, log: &mut Vec<(String, Color)>) {
+        if self.item.is_none() {
+            log.add(format!("Can't dequip {:?} because it's not an Item.", self),
+                colors::RED);
+            return
+        };
+        if let Some(ref mut equipment) = self.equipment {
+            if equipment.equipped {
+                equipment.equipped = false;
+                log.add(format!("Dequipped {} from {}.", self.name, equipment.slot),
+                    colors::LIGHT_YELLOW);
+            }
+        } else {
+            log.add(format!("Can't dequip {:?} because it's not an Equipment.", self),
+                colors::RED);
+        }
+    }
+
+    pub fn max_hp(&self, game: &Game) -> i32 {
+        let base_max_hp = self.fighter.map_or(0, |f| f.base_max_hp);
+        let bonus = self.get_all_equipped(game).iter().fold(0, |sum, e| sum + e.max_hp_bonus);
+        base_max_hp + bonus
+    }
+
+    pub fn power(&self, game: &Game) -> i32 {
+        let base_power = self.fighter.map_or(0, |f| f.base_power);
+        let bonus = self.get_all_equipped(game).iter().fold(0, |sum, e| sum + e.power_bonus);
+        base_power + bonus
+    }
+
+    pub fn defense(&self, game: &Game) -> i32 {
+        let base_defense = self.fighter.map_or(0, |f| f.base_defense);
+        let bonus = self.get_all_equipped(game).iter().fold(0, |sum, e| sum + e.defense_bonus);
+        base_defense + bonus
+    }
+
+    pub fn get_all_equipped(&self, game: &Game) -> Vec<Equipment> {
+        if self.name == "player" {
+            game.inventory
+                .iter()
+                .filter(|item| {
+                    item.equipment.map_or(false, |e| e.equipped)
+                })
+                .map(|item| item.equipment.unwrap())
+                .collect()
+        } else {
+            vec![]
+        }
+    }
+
+}
+
+// combat-related properties and methods (monster, player, NPC)
+#[derive(Clone, Copy, Debug, PartialEq, Serialize, Deserialize)]
+pub struct Fighter {
+    pub hp: i32,
+    pub base_max_hp: i32,
+    pub base_defense: i32,
+    pub base_power: i32,
+    pub on_death: DeathCallback,
+    pub xp: i32,
+}
+
+// a tile of the map and its properties
+#[derive(Clone, Copy, Debug, Serialize, Deserialize)]
+pub struct Tile {
+    pub blocked: bool,
+    pub block_sight: bool,
+    pub explored: bool,
+}
+
+impl Tile {
+    pub fn empty() -> Self {
+        Tile{blocked: false, block_sight: false, explored: false}
+    }
+
+    pub fn wall() -> Self {
+        Tile{blocked: true, block_sight: true, explored: false}
+    }
+}
+
+// a simple rectangle on the map, used to define a room
+#[derive(Clone, Copy, Debug)]
+pub struct Rect {
+    pub x1: i32,
+    pub y1: i32,
+    pub x2: i32,
+    pub y2: i32,
+}
+
+impl Rect {
+    pub fn new (x: i32, y: i32, w: i32, h: i32) -> Self {
+        Rect{x1: x, y1: y, x2: x + w, y2: y + h}
+    }
+
+    pub fn center(&self) -> (i32, i32) {
+        let center_x = (self.x1 + self.x2) / 2;
+        let center_y = (self.y1 + self.y2) / 2;
+        (center_x, center_y)
+    }
+
+    pub fn intersects_with(&self, other: &Rect) -> bool {
+        // return true if this rectangle intersects with another one
+        (self.x1 <= other.x2) && (self.x2 >= other.x1) &&
+            (self.y1 <= other.y2) && (self.y2 >= other.y1)
+    }
+}
+
+pub struct Transition {
+    pub level: u32,
+    pub value: u32,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Serialize, Deserialize)]
+/// an object that can be equipped, yielding bonuses.
+pub struct Equipment {
+    pub slot: Slot,
+    pub equipped: bool,
+    pub max_hp_bonus: i32,
+    pub power_bonus: i32,
+    pub defense_bonus: i32
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Game {
+    pub map: Map,
+    pub log: Messages,
+    pub inventory: Vec<Object>,
+    pub dungeon_level: u32,
+}
+
+pub struct Tcod {
+    pub root: Root,
+    pub con: Offscreen,
+    pub panel: Offscreen,
+    pub fov: FovMap,
+    pub mouse: Mouse,
+}
+
+/// types
+pub type Map = Vec<Vec<Tile>>; // a MAP is 2 dimensional vector of tiles
+
+// a Message is a tuple with 2 fields, message string and its color
+pub type Messages = Vec<(String, Color)>;
+
+/// enums
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub enum PlayerAction {
+    TookTurn,
+    DidntTakeTurn,
+    Exit,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Serialize, Deserialize)]
+pub enum Item {
+    Heal,
+    Lightning,
+    Confuse,
+    Fireball,
+    Sword,
+    Shield,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub enum UseResult {
+    UsedUp,
+    Cancelled,
+    UsedAndKept,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Serialize, Deserialize)]
+pub enum Slot {
+    LeftHand,
+    RightHand,
+    Head
+}
+
+impl std::fmt::Display for Slot {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match *self {
+            Slot::LeftHand => write!(f, "left hand"),
+            Slot::RightHand => write!(f, "right hand"),
+            Slot::Head => write!(f, "head"),
+        }
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+pub enum Ai {
+    Basic,
+    Confused{previous_ai: Box<Ai>, num_turns: i32},
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Serialize, Deserialize)]
+pub enum DeathCallback {
+    Player,
+    Monster,
+}
+
+// traits
+pub trait MessageLog {
+    fn add<T: Into<String>>(&mut self, message: T, color: Color);
+}
+
+impl MessageLog for Vec<(String, Color)> {
+    fn add<T: Into<String>>(&mut self, message: T, color: Color) {
+        self.push((message.into(), color));
+    }
+}
